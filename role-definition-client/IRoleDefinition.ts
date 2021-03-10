af4c5ae4d5d2d9067f61f09ec96c66cc11422397
John Henderson <john.henderson@energyweb.org>
2021-03-10 14:50:28 -0800

    initial RoleDefReader

---
+++ role-definition-client/IRoleDefinition.ts
@@ -0,0 +1,33 @@
+export interface IRoleDefinition extends IRoleDefinitionText {
+  version: string;
+  issuer: IIssuerDefinition
+  enrolmentPreconditions?: { type: PreconditionTypes; conditions: string[] }[];
+}
+
+export enum PreconditionTypes {
+  Role = "role"
+}
+
+export interface IRoleDefinitionText {
+  roleType: string;
+  roleName: string;
+  fields: {
+    fieldType: string;
+    label: string;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    pattern?: string;
+    minValue?: number;
+    maxValue?: number;
+    minDate?: Date;
+    maxDate?: Date;
+  }[];
+  metadata: Record<string, unknown> | Record<string, unknown>[];
+}
+
+export interface IIssuerDefinition {
+  issuerType?: string;
+  did?: string[];
+  roleName?: string;
+}
