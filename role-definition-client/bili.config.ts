0b0c7717d84ceabd861b1c9fb5c2c3c5f04c7865
John Henderson <john.henderson@energyweb.org>
2021-03-11 14:26:47 -0800

    removed comment from bili config

--- role-definition-client/bili.config.ts
+++ role-definition-client/bili.config.ts
@@ -3,9 +3,6 @@ import { Config } from 'bili';
 const config: Config = {
   plugins: {
     typescript2: {
-      // tsconfigOverride: {
-      //   include: ['src', 'ethers']
-      // }
     },
   },
   input: 'src/role-definition-client.ts',
