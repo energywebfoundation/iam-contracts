b535e8fcb0de4cd632b6d9868b4ceef801b93f47
John Henderson <john.henderson@energyweb.org>
2021-05-13 11:13:25 -0400

    docs(DomainNotifier): add purpose of contract

--- contracts/DomainNotifier.sol
+++ contracts/DomainNotifier.sol
@@ -2,6 +2,11 @@ pragma solidity 0.7.6;
 
 import "@ensdomains/ens/contracts/ENS.sol";
 
+/**
+ * DomainNotifier is used as a central point from which to
+ * determine which ENS domains (namespaces) have had their
+ * resolved data updated
+ */
 contract DomainNotifier {
     event DomainUpdated(bytes32 indexed node);
 
@@ -11,6 +16,11 @@ contract DomainNotifier {
         _ens = ens;
     }
 
+    /**
+     * Notifies of a domain/namespace's resolver data update.
+     * Only the resolver that is set for a given node should
+     * be able to trigger the notification
+     */
     function domainUpdated(bytes32 node) external {
         address resolver = _ens.resolver(node);
         require(resolver == msg.sender);
