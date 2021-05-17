4953fc288aaf1048a0ae10b77e41751f33cb4142
John Henderson <john.henderson@energyweb.org>
2021-05-17 16:18:08 -0400

    feat: add PreconditionType to package export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { DomainReader } from "./DomainReader"
 import { DomainTransactionFactory } from "./DomainTransactionFactory"
 import { getSubdomainsUsingResolver } from "./getSubDomains"
 import { addKnownResolver, setRegistryAddress } from "./resolverConfig";
-import { IAppDefinition, IOrganizationDefinition, IRoleDefinition, IRoleDefinitionText } from "./types/DomainDefinitions"
+import { IAppDefinition, IOrganizationDefinition, IRoleDefinition, IRoleDefinitionText, PreconditionType } from "./types/DomainDefinitions"
 import { ResolverContractType } from "./types/ResolverContractType";
 import { EncodedCall } from "./types/Transaction"
 
@@ -12,7 +12,7 @@ errors.setLogLevel("error");
 
 export { DomainReader }
 export { DomainTransactionFactory }
-export { IOrganizationDefinition, IAppDefinition, IRoleDefinition, IRoleDefinitionText }
+export { IOrganizationDefinition, IAppDefinition, IRoleDefinition, IRoleDefinitionText, PreconditionType }
 export { EncodedCall }
 export { addKnownResolver, setRegistryAddress }
 export { ResolverContractType }
