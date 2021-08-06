8ed3084015054a4a4d04386a1e5dbc6214d8b574
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-08-06 13:10:50 +0300

    build: dont export contract types
    BREAKING CHANGE:

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,20 @@
-import { errors } from "ethers";
+import { utils } from "ethers";
 import { DomainReader } from "./DomainReader"
 import { DomainTransactionFactory } from "./DomainTransactionFactory"
 import { DomainHierarchy } from "./DomainHierarchy"
 import { IAppDefinition, IOrganizationDefinition, IRoleDefinition, IRoleDefinitionText, PreconditionType } from "./types/DomainDefinitions"
 import { ResolverContractType } from "./types/ResolverContractType";
 import { EncodedCall } from "./types/Transaction"
-import { DomainNotifier__factory } from "../ethers-v4/factories/DomainNotifier__factory"
-import { ENSRegistry__factory } from "../ethers-v4/factories/ENSRegistry__factory"
-import { PublicResolver__factory } from "../ethers-v4/factories/PublicResolver__factory"
-import { RoleDefinitionResolver__factory } from "../ethers-v4/factories/RoleDefinitionResolver__factory"
-import { ClaimManager__factory } from "../ethers-v4/factories/ClaimManager__factory"
-import { StakingPool__factory } from "../ethers-v4/factories/StakingPool__factory"
-import { StakingPoolFactory__factory } from "../ethers-v4/factories/StakingPoolFactory__factory"
-import { RewardPool__factory } from "../ethers-v4/factories/RewardPool__factory"
 
 // To disable "WARNING: Multiple definitions for addr" that is triggered by ENS Registry
-errors.setLogLevel("error");
+const { Logger } = utils;
+Logger.setLogLevel(Logger.levels.ERROR);
 
 export { DomainReader }
 export { DomainTransactionFactory }
 export { IOrganizationDefinition, IAppDefinition, IRoleDefinition, IRoleDefinitionText, PreconditionType }
 export { EncodedCall }
 export * from "./chainConstants";
-export {PRINCIPAL_THRESHOLD, WITHDRAW_DELAY} from "./constants";
+export { PRINCIPAL_THRESHOLD, WITHDRAW_DELAY } from "./constants";
 export { ResolverContractType }
 export { DomainHierarchy }
-export {
-  DomainNotifier__factory,
-  ENSRegistry__factory,
-  PublicResolver__factory,
-  RoleDefinitionResolver__factory,
-  ClaimManager__factory,
-  StakingPool__factory,
-  StakingPoolFactory__factory,
-  RewardPool__factory
-}
