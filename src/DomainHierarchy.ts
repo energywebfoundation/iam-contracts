bc25a4107385fbba7bfe970e5641b662fb8cd64f
Jakub Sydor <jakub.sydor@gorrion.io>
2021-10-06 10:00:01 +0200

    refactor

--- src/DomainHierarchy.ts
+++ src/DomainHierarchy.ts
@@ -3,7 +3,7 @@ import { ENSRegistry } from "../ethers/ENSRegistry";
 import { ENSRegistry__factory } from "../ethers/factories/ENSRegistry__factory";
 import { abi as ensRegistryContract } from "../build/contracts/ENS.json";
 import { abi as ensResolverContract } from "../build/contracts/PublicResolver.json";
-import { abi as domainNotifierContract } from "../build/contracts/DomainNotifier.json";
+import { abi as domainNotifierContract } from '../build/contracts/DomainNotifier.json';
 import { emptyAddress } from "./constants";
 import { DomainReader } from "./DomainReader";
 import { PublicResolver__factory } from "../ethers/factories/PublicResolver__factory";
@@ -26,11 +26,11 @@ export class DomainHierarchy {
     domainNotifierAddress,
     publicResolverAddress,
   }: {
-    domainReader: DomainReader;
-    ensRegistryAddress: string;
-    provider: providers.Provider;
-    domainNotifierAddress: string;
-    publicResolverAddress?: string;
+    domainReader: DomainReader,
+    ensRegistryAddress: string,
+    provider: providers.Provider,
+    domainNotifierAddress: string
+    publicResolverAddress?: string,
   }) {
     if (!domainReader) throw new Error("You need to pass a DomainReader");
     this._domainReader = domainReader;
@@ -39,7 +39,7 @@ export class DomainHierarchy {
     if (!provider) throw new Error("You need to pass a provider");
     this._provider = provider;
     if (!domainNotifierAddress) throw new Error("You need to pass the address of a domain notifier contract");
-    this._domainNotifier = DomainNotifier__factory.connect(domainNotifierAddress, provider);
+    this._domainNotifier = DomainNotifier__factory.connect(domainNotifierAddress, provider)
 
     if (publicResolverAddress) {
       this._publicResolver = PublicResolver__factory.connect(publicResolverAddress, provider);
@@ -54,7 +54,7 @@ export class DomainHierarchy {
    */
   public getSubdomainsUsingResolver = async ({
     domain,
-    mode,
+    mode
   }: {
     domain: string;
     mode: "ALL" | "FIRSTLEVEL";
@@ -66,7 +66,7 @@ export class DomainHierarchy {
       const notRelevantDomainEndings = ["roles", "apps"];
       const leafLabel = name.split(".")[0];
       return notRelevantDomainEndings.includes(leafLabel);
-    };
+    } 
 
     if (mode === "ALL") {
       const getParser = (nameReader: (node: string) => Promise<string>) => {
@@ -74,7 +74,7 @@ export class DomainHierarchy {
           try {
             const name = await nameReader(node);
             if (isRelevantDomainEndings(name)) {
-              return "";
+              return ""
             }
 
             if (name.endsWith(domain) && name !== domain) {
@@ -87,20 +87,20 @@ export class DomainHierarchy {
             return "";
           }
           return "";
-        };
-      };
+        }
+      }
       let subDomains = await this.getDomainsFromLogs({
         parser: getParser(this._domainReader.readName.bind(this._domainReader)),
         provider: this._domainNotifier.provider,
         event: this._domainNotifier.filters.DomainUpdated(null),
-        contractInterface: new utils.Interface(domainNotifierContract),
+        contractInterface: new utils.Interface(domainNotifierContract)
       });
       if (this._publicResolver) {
         const publicResolverDomains = await this.getDomainsFromLogs({
           parser: getParser(this._publicResolver.name),
           provider: this._publicResolver.provider,
           event: this._publicResolver.filters.TextChanged(null, "metadata", null),
-          contractInterface: new utils.Interface(ensResolverContract),
+          contractInterface: new utils.Interface(ensResolverContract)
         });
         subDomains = new Set([...publicResolverDomains, ...subDomains]);
       }
@@ -115,18 +115,19 @@ export class DomainHierarchy {
         try {
           const [name, ownerAddress] = await Promise.all([
             this._domainReader.readName(namehash),
-            this._ensRegistry.owner(namehash),
+            this._ensRegistry.owner(namehash)
           ]);
           if (ownerAddress === emptyAddress) return "";
           if (isRelevantDomainEndings(name)) {
-            return "";
+              return ""
           }
           return name;
-        } catch {
+        }
+        catch {
           return "";
         }
       },
-      provider: this._ensRegistry.provider,
+      provider: this._ensRegistry.provider
     });
     return [...singleLevel].filter(Boolean); // Boolean filter to remove empty string
   };
@@ -137,16 +138,20 @@ export class DomainHierarchy {
    * For multi-level queries with many domains, querying the registry is slower than
    * using the resolver contract because of the repeated RPC call.
    */
-  public getSubdomainsUsingRegistry = async ({ domain }: { domain: string }): Promise<string[]> => {
+  public getSubdomainsUsingRegistry = async ({
+    domain,
+  }: {
+    domain: string;
+  }): Promise<string[]> => {
     if (!domain) throw new Error("You need to pass a domain name");
-    const notRelevantDomainEndings = ["roles", "apps"];
+    const notRelevantDomainEndings = ["roles", "apps"]
     const parser = async ({ node, label, owner }: Result) => {
       try {
         if (owner === emptyAddress) return "";
         const namehash = utils.keccak256(node + label.slice(2));
         const [name, ownerAddress] = await Promise.all([
           this._domainReader.readName(namehash),
-          this._ensRegistry.owner(namehash),
+          this._ensRegistry.owner(namehash)
         ]);
         if (ownerAddress === emptyAddress) return "";
         return name;
@@ -154,36 +159,36 @@ export class DomainHierarchy {
         // A possible source of exceptions is if domain has been deleted (https://energyweb.atlassian.net/browse/SWTCH-997)
         return "";
       }
-    };
-    const queue: string[][] = [];
+    }
+    const queue: string[][] = []
     const subDomains: Set<string> = new Set();
-    queue.push([domain]);
-    subDomains.add(domain);
+    queue.push([domain])
+    subDomains.add(domain)
 
     // Breadth-first search down subdomain tree
     while (queue.length > 0) {
-      const currentNodes = queue[0];
-      const currentNameHashes = currentNodes.map((node) => utils.namehash(node));
-      const event = this._ensRegistry.filters.NewOwner(null, null, null);
+      const currentNodes = queue[0]
+      const currentNameHashes = currentNodes.map(node => utils.namehash(node));
+      const event = this._ensRegistry.filters.NewOwner(null, null, null)
       // topics should be able to accept an array: https://docs.ethers.io/v5/concepts/events/
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      event.topics[1] = currentNameHashes;
+      event.topics[1] = currentNameHashes
       const uniqueDomains = await this.getDomainsFromLogs({
         provider: this._ensRegistry.provider,
         parser,
         event,
-        contractInterface: new utils.Interface(ensRegistryContract),
-      });
+        contractInterface: new utils.Interface(ensRegistryContract)
+      })
       if (uniqueDomains.size > 0) {
-        queue.push([...uniqueDomains]);
+        queue.push([...uniqueDomains])
       }
       for (const domain of uniqueDomains) {
-        const leafLabel = domain.split(".")[0];
-        if (notRelevantDomainEndings.includes(leafLabel)) continue;
-        subDomains.add(domain);
+        const leafLabel = domain.split(".")[0]
+        if (notRelevantDomainEndings.includes(leafLabel)) continue
+        subDomains.add(domain)
       }
-      queue.shift();
+      queue.shift()
     }
     return [...subDomains].filter(Boolean); // Boolean filter to remove empty string
   };
@@ -192,7 +197,7 @@ export class DomainHierarchy {
     provider,
     parser,
     event,
-    contractInterface,
+    contractInterface
   }: {
     provider: providers.Provider;
     parser: (log: Result) => Promise<string>;
@@ -203,16 +208,16 @@ export class DomainHierarchy {
       fromBlock: 0,
       toBlock: "latest",
       address: event.address,
-      topics: event.topics || [],
+      topics: event.topics || []
     };
     const logs = await provider.getLogs(filter);
-    const rawLogs = logs.map((log) => {
+    const rawLogs = logs.map(log => {
       const parsedLog = contractInterface.parseLog(log);
       /** ethers_v5 Interface.parseLog incorrectly parses log, so have to use lowlevel alternative */
       return contractInterface.decodeEventLog(parsedLog.name, log.data, log.topics);
     });
     const domains = await Promise.all(rawLogs.map(parser));
-    const nonEmptyDomains = domains.filter((domain) => domain != "");
+    const nonEmptyDomains = domains.filter(domain => domain != '');
     return new Set(nonEmptyDomains);
   };
 }
