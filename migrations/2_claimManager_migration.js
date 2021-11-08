12d1c8501713eae727d97650ae3556be480b6fde
nichonien <ashish.tripathi@energyweb.org>
2021-11-08 14:50:12 +0530

    ICL-130/update migrations

--- migrations/2_claimManager_migration.js
+++ migrations/2_claimManager_migration.js
@@ -1,7 +1,7 @@
-// const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
-// const ClaimManager = artifacts.require('ClaimManager');
+const ClaimManager = artifacts.require('ClaimManager');
 
-// module.exports = async (deployer) => {
-//     await deployProxy(ClaimManager, [erc1056, ensRegistry], { deployer });
-// };
+module.exports = async (deployer) => {
+    await deployProxy(ClaimManager, ["0x84d0c7284A869213CB047595d34d6044d9a7E14A", "0xd7CeF70Ba7efc2035256d828d5287e2D285CD1ac"], { deployer });
+};
