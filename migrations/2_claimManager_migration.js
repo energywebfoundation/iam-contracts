13e06c21076e96be9dcf43e34f6c52e4dd7cea7d
nichonien <ashish.tripathi@energyweb.org>
2021-11-09 15:43:23 +0530

    add const declaration for reuse

--- migrations/2_claimManager_migration.js
+++ migrations/2_claimManager_migration.js
@@ -1,7 +1,9 @@
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
+const VOLTA_ERC1056 = "0xc15d5a57a8eb0e1dcbe5d88b8f9a82017e5cc4af";
+const VOLTA_ENS_REGISTRY = "0xd7CeF70Ba7efc2035256d828d5287e2D285CD1ac";
 const ClaimManager = artifacts.require('ClaimManager');
 
 module.exports = async (deployer) => {
-    await deployProxy(ClaimManager, ["0x84d0c7284A869213CB047595d34d6044d9a7E14A", "0xd7CeF70Ba7efc2035256d828d5287e2D285CD1ac"], { deployer });
+    await deployProxy(ClaimManager, [VOLTA_ERC1056, VOLTA_ENS_REGISTRY], { deployer });
 };
