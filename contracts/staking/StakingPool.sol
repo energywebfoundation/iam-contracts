cddbb18b85b241eb0793a24978cd24d4dbc03b8c
Jakub Sydor <sydorjakub@gmail.com>
2022-01-28 12:44:01 +0100

    feat(staking): revert remove staking contracts

---
+++ contracts/staking/StakingPool.sol
@@ -0,0 +1,167 @@
+pragma solidity 0.8.6;
+import "../roles/ClaimManager.sol";
+import "./RewardPool.sol";
+
+contract StakingPool {
+  struct Stake {
+    uint amount;
+    uint depositStart;
+    uint depositEnd;
+    StakeStatus status;
+  }
+  
+  enum StakeStatus { NONSTAKING, STAKING, WITHDRAWING }
+  
+  event StakePut(address indexed patron, uint indexed amount, uint indexed timestamp);
+  event StakeWithdrawalRequested(address indexed patron, uint indexed timestamp);
+  event StakeWithdrawn(address indexed patron, uint indexed timestamp);
+  
+  uint principal;
+  uint public totalStake;
+  uint immutable public minStakingPeriod; // seconds
+
+  uint immutable public withdrawDelay; // seconds
+  
+  address immutable claimManager;
+  bytes32[] patronRoles;
+  
+  address payable immutable rewardPool;
+  uint immutable patronRewardPortion;
+  
+  mapping(address => Stake) public stakes;
+  address[] patrons;
+  mapping(address => uint) indexOfPatron;
+  
+  constructor(
+    uint _minStakingPeriod,
+    uint _withdrawDelay,
+    address _claimManager,
+    bytes32[] memory _patronRoles,
+    address payable _rewardPool,
+    uint _patronRewardPortion
+  ) payable {
+    minStakingPeriod = _minStakingPeriod;
+    withdrawDelay = _withdrawDelay;
+    claimManager = _claimManager;
+    patronRoles = _patronRoles;
+    
+    principal = msg.value;
+    totalStake = principal;
+    
+    rewardPool = _rewardPool;
+    patronRewardPortion = _patronRewardPortion;
+  }
+  
+  function hasPatronRole() internal returns (bool) {
+    if (patronRoles.length == 0) {
+      return true;
+    }
+    ClaimManager cm = ClaimManager(claimManager);
+    for (uint i = 0; i < patronRoles.length; i++) {
+      if (cm.hasRole(msg.sender, patronRoles[i], 0)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function putStake() payable external {
+    require(
+      hasPatronRole(),
+      "StakingPool: patron is not registered with patron role"
+    );
+    address patron = msg.sender;
+    Stake storage stake = stakes[patron];
+    require(
+      stake.status == StakeStatus.NONSTAKING,
+      "StakingPool: Replenishment of the stake is not allowed"
+    );
+    uint amount = msg.value;
+    require(amount > 0, "StakingPool: stake amount is not provided");
+    uint depositStart = block.timestamp;
+    stake.amount = amount;
+    stake.depositStart = depositStart;
+    stake.depositEnd = depositStart;
+    stake.status = StakeStatus.STAKING;
+    totalStake += stake.amount;
+    addPatron(patron);
+    
+    emit StakePut(patron, stake.amount, stake.depositStart);
+  }
+  
+  /**
+  * @dev stops staking and notifies reward pool
+   */
+  function requestWithdraw() external {
+    address patron = msg.sender;
+    Stake storage stake = stakes[patron];
+    require(
+      stake.status == StakeStatus.STAKING,
+      "StakingPool: No stake to withdraw"
+    );
+    require(
+      block.timestamp >= stake.depositStart + minStakingPeriod,
+       "StakingPool: Minimum staking period is not expired yet"
+    );
+    stake.status = StakeStatus.WITHDRAWING;
+    stake.depositEnd = block.timestamp;
+    
+    emit StakeWithdrawalRequested(patron, stake.depositEnd);
+  }
+  
+  /**
+  * @dev invoked after expiring of withdraw delay
+   */
+  function withdraw() external {
+    address patron = msg.sender;
+    Stake storage stake = stakes[patron];
+    require(
+      stake.status == StakeStatus.WITHDRAWING, 
+      "StakingPool: Stake hasn't requested to withdraw"
+    );
+    require(
+      block.timestamp >= stake.depositEnd + withdrawDelay,
+      "StakingPool: Withdrawal delay hasn't expired yet"
+    );
+    RewardPool(rewardPool).payReward(payable(patron), stake.amount, stake.depositEnd - stake.depositStart, patronRewardPortion);
+    payable(patron).transfer(stake.amount);   
+    totalStake -= stake.amount;
+    delete stakes[patron];
+    removePatron(patron);
+    
+    emit StakeWithdrawn(patron, block.timestamp);
+  }
+  
+  function checkReward() public view returns (uint reward) {
+    Stake storage stake = stakes[msg.sender];
+    require(
+      stake.status != StakeStatus.NONSTAKING,
+      "StakingPool: No stake"
+    );
+    if (stake.status == StakeStatus.STAKING) {
+      reward = RewardPool(rewardPool).checkReward(stake.amount, block.timestamp - stake.depositStart, patronRewardPortion);
+    }
+    else {
+      reward = RewardPool(rewardPool).checkReward(stake.amount, stake.depositEnd - stake.depositStart, patronRewardPortion);
+    }
+  }
+  
+  function addPatron(address _patron) internal {
+    patrons.push(_patron);
+    indexOfPatron[_patron] = patrons.length - 1;
+  }
+  
+  function removePatron(address _patron) internal {
+    uint index = indexOfPatron[_patron];
+    require(
+      patrons[index] == _patron,
+      "StakingPool: No such patron"
+    );
+    
+    if (index > 0 && index < patrons.length - 1) {
+      patrons[index] = patrons[patrons.length-1];
+      indexOfPatron[patrons[index]] = index;
+    }
+    patrons.pop();
+  }
+}
---
+++ test/staking/StakingPool.testSuite.ts
@@ -0,0 +1,286 @@
+import { Signer, utils } from 'ethers';
+import { expect } from 'chai';
+import { requestRole } from '../test_utils/role_utils';
+import {
+  claimManager,
+  waitFor,
+  ewc,
+  patronRole,
+  stakingPoolFactory,
+  serviceProvider,
+  org,
+  defaultMinStakingPeriod,
+  defaultWithdrawDelay,
+  getSigner,
+} from './staking.testSuite';
+import { StakingPool } from '../../ethers/StakingPool';
+import { StakingPool__factory } from '../../ethers/factories/StakingPool__factory';
+
+const { namehash, parseEther } = utils;
+
+export function stakingPoolTests(): void {
+  let patron: Signer;
+  let stakingPool: StakingPool;
+  const amount = parseEther('0.1');
+  const version = 1;
+  const patronRewardPortion = 80;
+  const principal = parseEther('0.2');
+
+  async function setupStakingPool({
+    patronRoles = [patronRole],
+    minStakingPeriod = defaultMinStakingPeriod,
+  } = {}) {
+    await (
+      await stakingPoolFactory.connect(serviceProvider).launchStakingPool(
+        namehash(org),
+        minStakingPeriod,
+        patronRewardPortion,
+        patronRoles.map((r) => namehash(r)),
+        { value: principal }
+      )
+    ).wait();
+    const stakingPoolAddr = (await stakingPoolFactory.services(namehash(org)))
+      .pool;
+    stakingPool = new StakingPool__factory(patron).attach(stakingPoolAddr);
+  }
+
+  beforeEach(async () => {
+    patron = await getSigner();
+  });
+
+  it('initial stakes consists only from principal', async () => {
+    await setupStakingPool();
+    expect((await stakingPool.totalStake()).eq(principal)).true;
+  });
+
+  it('should not be possible to put a stake without having patron role', async () => {
+    await setupStakingPool();
+
+    return expect(stakingPool.putStake({ value: amount })).rejectedWith(
+      'StakingPool: patron is not registered with patron role'
+    );
+  });
+
+  it('should be possible to stake when no patron roles are required', async () => {
+    await setupStakingPool({ patronRoles: [] });
+
+    const stakePut = waitFor(
+      stakingPool.filters.StakePut(await patron.getAddress(), amount, null),
+      stakingPool
+    );
+
+    stakingPool.connect(patron).putStake({ value: amount });
+
+    return expect(stakePut).fulfilled;
+  });
+
+  it('having patron role should be able to put a stake', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+
+    const stakePut = waitFor(
+      stakingPool.filters.StakePut(await patron.getAddress(), amount, null),
+      stakingPool
+    );
+
+    stakingPool.connect(patron).putStake({ value: amount });
+
+    return expect(stakePut).fulfilled;
+  });
+
+  it('putting stake should increase total stake', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+
+    expect((await stakingPool.totalStake()).eq(principal.add(amount))).true;
+  });
+
+  it('should not be possible to replenish stake', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+
+    return expect(
+      stakingPool.connect(patron).putStake({ value: amount })
+    ).rejectedWith('StakingPool: Replenishment of the stake is not allowed');
+  });
+
+  it('should not be possible to request withdraw before minimal staking period is expired', async () => {
+    await setupStakingPool({ minStakingPeriod: 1000 });
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+
+    return expect(stakingPool.connect(patron).requestWithdraw()).rejectedWith(
+      'StakingPool: Minimum staking period is not expired yet'
+    );
+  });
+
+  it('should be able to request withdraw after minimal staking period is expired', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+
+    const withdrawnRequested = waitFor(
+      stakingPool.filters.StakeWithdrawalRequested(
+        await patron.getAddress(),
+        null
+      ),
+      stakingPool
+    );
+    stakingPool.connect(patron).requestWithdraw();
+
+    return expect(withdrawnRequested).fulfilled;
+  });
+
+  it('should not be possible to repeat withdrawal request', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+
+    await stakingPool.connect(patron).requestWithdraw();
+
+    return expect(stakingPool.connect(patron).requestWithdraw()).rejectedWith(
+      'StakingPool: No stake to withdraw'
+    );
+  });
+
+  it('should not be possible to withdraw before withdraw delay is expired', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+    await stakingPool.connect(patron).requestWithdraw();
+
+    return expect(stakingPool.connect(patron).withdraw()).rejectedWith(
+      "StakingPool: Withdrawal delay hasn't expired yet"
+    );
+  });
+
+  it('should be able to withdraw after withdraw delay is expired', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+    await stakingPool.connect(patron).requestWithdraw();
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultWithdrawDelay);
+    });
+
+    const withdrawn = waitFor(
+      stakingPool.filters.StakeWithdrawn(await patron.getAddress(), null),
+      stakingPool
+    );
+    stakingPool.connect(patron).withdraw();
+
+    return expect(withdrawn).fulfilled;
+  });
+
+  it('stake withdrawal should reduce total stake', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+
+    expect((await stakingPool.totalStake()).eq(principal.add(amount))).true;
+
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+    await stakingPool.connect(patron).requestWithdraw();
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultWithdrawDelay);
+    });
+    await stakingPool.connect(patron).withdraw();
+
+    expect((await stakingPool.totalStake()).eq(principal)).true;
+  });
+
+  it('should not be possible to repeat withdraw', async () => {
+    await setupStakingPool();
+    await requestRole({
+      claimManager,
+      roleName: patronRole,
+      version,
+      agreementSigner: patron,
+      proofSigner: ewc,
+    });
+    await stakingPool.connect(patron).putStake({ value: amount });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultMinStakingPeriod);
+    });
+    await stakingPool.connect(patron).requestWithdraw();
+    await new Promise((resolve) => {
+      setTimeout(resolve, 2 * 1000 * defaultWithdrawDelay);
+    });
+
+    await stakingPool.connect(patron).withdraw();
+
+    return expect(stakingPool.connect(patron).withdraw()).rejectedWith(
+      "StakingPool: Stake hasn't requested to withdraw"
+    );
+  });
+}
